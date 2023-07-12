9cfb6da83f24eefa024197cdcb235b6ec3e99fad
Amrutharavindran <116252422+Amrutharavindran@users.noreply.github.com>
2023-07-12 11:41:53 +0530

    Delete words.sh

--- words.sh
+++
@@ -1,9 +0,0 @@
-#program to count the number of lines and words of a file
-#! /bin/bash
-echo "enter a file:"
-read file
-w=`cat $file | wc -w`
-l=`grep -c "." $file`
-echo "number of words in file is :"$w
-echo "number of lines in file is:"$l 
-
