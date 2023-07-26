bad77c1a7dd7ca2e5ea10a08ac3bf42a79401387
Amrutharavindran <116252422+Amrutharavindran@users.noreply.github.com>
2023-07-26 14:10:05 +0530

    Update reverse.sh

--- reverse.sh
+++ reverse.sh
@@ -17,3 +17,28 @@ mv temp file
 done
 cat rfile
 exit 0
+****************************************************************************8
+output
+ksb@ksb-H410M-S2-V2:~/networking$ sh reverse.sh
+syntax is <reverse.sh> <filename>
+
+ksb@ksb-H410M-S2-V2:~/networking$ sh reverse.sh reverse.sh
+exit 0
+cat rfile
+done
+mv temp file
+head -n $l file > temp
+l=`expr $l - 1`
+tail -n 1 file >> rfile
+do
+while [ $l -ge 1 ] 
+rm rfile
+l=`cat file | wc -l`
+cp $1 file
+fi 
+exit 1
+echo "syntax is <$0> <filename>"
+then
+if [ $# -ne 1 ]
+#set -xv
+#to reverse a file
