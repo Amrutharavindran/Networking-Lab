09119c5c44867210151bc5a7011198060cf6ae85
Amrutharavindran <116252422+Amrutharavindran@users.noreply.github.com>
2023-07-26 14:12:33 +0530

    Update triangle.sh

--- triangle.sh
+++ triangle.sh
@@ -20,4 +20,29 @@ then
 else
     echo "$a $b $c can be the sides of a triangle"
 fi
- 
+***********************************************************************************************
+output
+
+ksb@ksb-H410M-S2-V2:~/networking$ sh triangle.sh
+enter first number
+5
+enter second number
+6
+enter third number
+9
+output
+5 6 9 can be the sides of a triangle
+
+ksb@ksb-H410M-S2-V2:~/networking$ sh triangle.sh
+enter first number
+5
+enter second number
+3
+enter third number
+2
+output
+5 3 2 cannot be the sides of a triangle 
+
+ksb@ksb-H410M-S2-V2:~/networking$ sh triangle.sh 4 5 6
+#syntax error  <triangle.sh>
+
