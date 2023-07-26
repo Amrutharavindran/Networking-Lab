249d276f85995db3125d2048e72afb0241f2ef35
Amrutharavindran <116252422+Amrutharavindran@users.noreply.github.com>
2023-07-26 14:04:08 +0530

    Update modify.sh

--- modify.sh
+++ modify.sh
@@ -19,4 +19,23 @@ fi
 fi
 cp $1 $2
 echo "file copied"
+*****************************************************************************8
+output
+
+ksb@ksb-H410M-S2-V2:~/networking$ sh modify.sh
+ syntax is <modify.sh><src filename><tgt filename>
+ 
+ksb@ksb-H410M-S2-V2:~/networking$ sh modify.sh zz xy
+zz is not existing or not an ordinary file
+
+ksb@ksb-H410M-S2-V2:~/networking$ sh modify.sh mca.txt li
+target file exist,overwrite it y/n
+y
+file copied
+
+ksb@ksb-H410M-S2-V2:~/networking$ sh modify.sh mca.txt li
+target file exist,overwrite it y/n
+n
+ksb@ksb-H410M-S2-V2:~/networking$ 
+
 
